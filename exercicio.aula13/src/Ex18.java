1b8475cf286ddcc9e95f393a233ccca742992ddc
Anderson Santos <andersonacpemp@gmail.com>
2025-07-01 20:17:33 -0300

    Add new tasks

--- exercicio.aula13/src/Ex18.java
+++ exercicio.aula13/src/Ex18.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Ex18 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int mbs = scan.nextInt();
+        int mbps = scan.nextInt();
+        System.out.println("time required: " + (mbs / mbps) / 60);
+    }
 }
