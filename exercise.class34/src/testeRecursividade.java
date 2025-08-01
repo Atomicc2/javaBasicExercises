0fc2339d508f65e66704f8128623d8581ebde403
Anderson S. <andersonacpemp@gmail.com>
2025-08-01 07:52:34 -0300

    Add test recursion

--- exercise.class34/src/testeRecursividade.java
+++ exercise.class34/src/testeRecursividade.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class testeRecursividade {
+    public static int fibonacci(int n) {
+        if(n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(fibonacci(n));
+    }
 }
